a5657dfe16ce8ba6fe03970e910855a0645cbe20
Janik Mayr <jan.222.ik@gmail.com>
2020-05-02 18:23:12 +0200

    First render of board

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
+import {sudoku} from './lib/sudoku.js'
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {IconChart, IconSearch} from "./Icons";
 
 const darkTheme = createMuiTheme({
     palette: {
@@ -13,19 +10,31 @@ const darkTheme = createMuiTheme({
     },
 });
 
+const split = (board: [[string], [string],[string],[string],[string],[string],[string],[string],[string]]) => {
+    return [
+        [ // Box 0, 0
+            board[0].slice(0,  3),
+            board[1].slice(0, 3),
+            board[2].slice(0, 3)
+        ]
+    ]
+}
+
 const App = () => {
-    const [currentTab, setCurrentTab] = useState(0);
-    return (
-        <>
-            <AppBar position="sticky" color="default">
-                <Tabs value={currentTab} onChange={(_, newVal) => setCurrentTab(newVal)}
-                      variant="fullWidth" indicatorColor="primary" textColor="secondary">
-                    <Tab label="Trending" icon={IconChart}/>
-                    <Tab label="Search" icon={IconSearch}/>
-                </Tabs>
-            </AppBar>
-        </>
-    );
+    const boardString = sudoku.generate("easy");
+    const boardStringToGrid = sudoku.board_string_to_grid(boardString);
+    console.log(boardString);
+    console.table(split(boardStringToGrid))
+    //state currNum
+    return <>
+        {
+            boardStringToGrid.map(row => {
+                return <div>{row.map(col => {
+                    return <span>{col}</span>
+                })}</div>
+            })
+        }
+    </>;
 };
 
 ReactDOM.render(
